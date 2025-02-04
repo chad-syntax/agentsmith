bbd0e1b942f687bf2912f637f1a41781eabffde2
Chad $yntax <chad@ttpspodcast.com>
2025-02-03 19:37:07 -0500

    adjust html title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Agentsmith - AI Agent Development Platform',
+  description:
+    'Agentsmith is the fastest way to build and iterate on LLM-powered apps',
 };
 
 const robotoMono = Roboto_Mono({
