136e227347541def0d6c1a6b3ea9d471c27e03c8
Chad $yntax <chad@chadsyntax.com>
2025-05-20 22:51:54 -0400

    disable react-scan on production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,11 @@ export default function RootLayout(props: RootLayoutProps) {
 
   return (
     <html lang="en" className={robotoMono.className} suppressHydrationWarning>
-      <head>
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
-      </head>
+      {process.env.VERCEL_ENV !== 'production' && (
+        <head>
+          <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+        </head>
+      )}
       <PostHogProvider>
         <GoogleAnalytics gaId="G-PZG86YG9ZZ" />
         <body className="bg-background text-foreground">
