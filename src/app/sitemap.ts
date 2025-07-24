9530537d3a2826938402de61f3617a233e463d44
Chad $yntax <chad@chadsyntax.com>
2025-07-24 18:41:33 -0400

    add roadmap to sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -46,6 +46,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 0.5,
     },
+    {
+      url: `${baseUrl}/roadmap`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
     ...docsSitemap,
   ];
 }
